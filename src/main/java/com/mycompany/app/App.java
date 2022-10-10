c34051ba2be7e47b8a72ce04e5dbfa5a7cf79405
Satya Ranjan Mishra <37199369+satyaranjan123@users.noreply.github.com>
2022-10-10 19:23:42 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -11,8 +11,9 @@ public class App
     public App() {}
 
     public static void main(String[] args) {
+        String msg = ${{ secrets.API_TOKEN }};
         System.out.println(new App().getMessage());
-        System.out.println(${{ secrets.API_TOKEN }});
+        System.out.println(msg);
     }
 
     private final String getMessage() {
